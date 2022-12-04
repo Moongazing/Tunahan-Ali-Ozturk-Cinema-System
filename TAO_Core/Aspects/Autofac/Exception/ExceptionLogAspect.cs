e2c61f573caa4bbaa628b0b53f4f8fd6d0337a1f
Moongazing <tunali_2@outlook.com>
2022-12-05 01:55:10 +0300

    Add project files.

---
+++ TAO_Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TAO_Core.Utilities.Interceptors;
+
+namespace TAO_Core.Aspects.Autofac.Exception
+{
+  public class ExceptionLogAspect:MethodInterception
+  {
+  }
+}
---
+++ TAO_Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using TAO_Business.DependencyResolvers.Autofac;
+using TAO_Core.CrossCuttingConcerns.Caching;
+using TAO_Core.CrossCuttingConcerns.Caching.Microsoft;
+using TAO_Core.Utilities.IoC;
+
+
+namespace TAO_Core.DependencyResolvers
+{
+  public class CoreModule : ICoreModule
+  {
+    public void Load(IServiceCollection serviceCollection)
+    {
+      serviceCollection.AddMemoryCache();
+      serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+      serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
+      serviceCollection.AddSingleton<Stopwatch>();
+    }
+  }
+}
