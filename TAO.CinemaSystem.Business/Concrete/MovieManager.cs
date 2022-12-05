475f3d4f4c40a02bc1dfce6df1df1b5e5ab04862
Moongazing <tunali_2@outlook.com>
2022-12-05 10:49:09 +0300

    Adding Aspects

--- TAO.CinemaSystem.Business/Concrete/MovieManager.cs
+++ TAO.CinemaSystem.Business/Concrete/MovieManager.cs
@@ -6,6 +6,7 @@ using TAO.CinemaSystem.Business.Constans;
 using TAO.CinemaSystem.Business.ValidationRules.FluentValidation;
 using TAO.CinemaSystem.DataAccess.Abstract;
 using TAO.CinemaSystem.Entities.Concrete;
+using TAO_Core.Aspects.Autofac.Caching;
 using TAO_Core.Aspects.Autofac.Validation;
 using TAO_Core.Utilities.Results;
 using TAO_Core.Utilities.Results.Abstract;
@@ -20,7 +21,10 @@ namespace TAO.CinemaSystem.Business.Concrete
     {
       _movieDal = movieDal;
     }
+
     [ValidationAspect(typeof(MovieValidator))]
+    [CacheRemoveAspect("IMovieService.Get")]
+
     public IResult Add(Movie movie)
     {
       _movieDal.Add(movie);
@@ -32,51 +36,50 @@ namespace TAO.CinemaSystem.Business.Concrete
       _movieDal.Delete(movie);
       return new SuccessResult(Messages.MovieDeletedSuccessfly);
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetAll()
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(),Messages.MoviesListedSuccessfly);
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetByDirector(string director)
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(m=>m.Director == director), Messages.MoviesListedSuccessfly);
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetByDuration(DateTime duration)
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(m=>m.Duration == duration), Messages.MoviesListedSuccessfly);
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetByHall(string hall)
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(m=>m.Hall == hall), Messages.MoviesListedSuccessfly);
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetByMovieName(string movieName)
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(m=>m.Name == movieName), Messages.MoviesListedSuccessfly);
-
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetByPublisher(string publisher)
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(m=>m.Publisher == publisher), Messages.MoviesListedSuccessfly);
 
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetByReleaseDate(DateTime releaseDate)
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(m=>m.ReleaseDate == releaseDate), Messages.MoviesListedSuccessfly);
 
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetByVisionDate(DateTime visionDate)
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(m=>m.VisionDate == visionDate), Messages.MoviesListedSuccessfly);
 
     }
-
+    [CacheAspect]
     public IDataResult<List<Movie>> GetByVisionEndDate(DateTime visionEndDate)
     {
       return new SuccessDataResult<List<Movie>>(_movieDal.GetAll(m=>m.VisionEndDate == visionEndDate), Messages.MoviesListedSuccessfly);
